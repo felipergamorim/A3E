8e191c1b5bda73e19d9fe1fbd51c64f057928a67
felipergamorim <feliperibeirogomesamorim@gmail.com>
2018-05-29 12:27:45 -0300

    Criação das tabelas do banco de dados

--- src/providers/database/database.ts
+++ src/providers/database/database.ts
@@ -40,7 +40,19 @@ export class DatabaseProvider {
   private createTables(db: SQLiteObject) {
     // Criando as tabelas
     db.sqlBatch([
-      ['CREATE TABLE IF NOT EXISTS escolas (id integer primary key AUTOINCREMENT NOT NULL, nome TEXT)']
+      ['CREATE TABLE IF NOT EXISTS escolas (escola_id integer primary key AUTOINCREMENT NOT NULL, nome TEXT)'],
+      ['CREATE TABLE IF NOT EXISTS classes (classe_id integer primary key AUTOINCREMENT NOT NULL, nome TEXT)'],
+      ['CREATE TABLE IF NOT EXISTS tipos (tipo_id integer primary key AUTOINCREMENT NOT NULL, classe_id integer, nome TEXT)'],
+      ['CREATE TABLE IF NOT EXISTS avaliaveis (avaliavel_id integer primary key AUTOINCREMENT NOT NULL, escola_id integer, classe_id integer, tipo_id integer, identificacao TEXT, obs TEXT)'],
+      ['CREATE TABLE IF NOT EXISTS perguntas (pergunta_id integer primary key AUTOINCREMENT NOT NULL, tipo_id integer, pergunta TEXT)'],      
+      ['CREATE TABLE IF NOT EXISTS respostas (resposta_id integer primary key AUTOINCREMENT NOT NULL, avaliavel_id integer, pergunta_id integer, resposta CHAR(1))'],
+      ['ALTER TABLE tipos ADD FOREIGN KEY(classe_id) REFERENCES classes(classe_id)'],
+      ['ALTER TABLE avaliaveis ADD FOREIGN KEY(escola_id) REFERENCES escolas(escola_id)'],
+      ['ALTER TABLE avaliaveis ADD FOREIGN KEY(classe_id) REFERENCES classes(classe_id)'],
+      ['ALTER TABLE avaliaveis ADD FOREIGN KEY(tipo_id) REFERENCES tipos(tipo_id)'],
+      ['ALTER TABLE perguntas ADD FOREIGN KEY(tipo_id) REFERENCES tipos(tipo_id)'],
+      ['ALTER TABLE respostas ADD FOREIGN KEY(pergunta_id) REFERENCES perguntas(pergunta_id)'],
+      ['ALTER TABLE respostas ADD FOREIGN KEY(avaliavel_id) REFERENCES avaliaveis(avaliavel_id)']
     ])
       .then(() => console.log('Tabelas criadas'))
       .catch(e => console.error('Erro ao criar as tabelas', e));
@@ -58,7 +70,12 @@ export class DatabaseProvider {
 
         // Criando as tabelas
         db.sqlBatch([
-          ['insert into escolas (name) values (?)', ['Instituto Federal Fluminense Campus Campos Centro']]
+          ['insert into escolas (nome) values (?)', ['Instituto Federal Fluminense Campus Campos Centro']],
+          ['insert into classes (classe_id,nome) values (?,?)', [1,'Entorno']],
+          ['insert into classes (classe_id,nome) values (?,?)', [2,'Circulação']],
+          ['insert into classes (classe_id,nome) values (?,?)', [3,'Acessos e Estacionamento']],
+          ['insert into classes (classe_id,nome) values (?,?)', [4,'Ambientes']],
+          ['insert into classes (classe_id,nome) values (?,?)', [5,'Informações e Sinalização']]
         ])
           .then(() => console.log('Dados padrões incluídos'))
           .catch(e => console.error('Erro ao incluir dados padrões', e));
